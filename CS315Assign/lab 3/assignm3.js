a62bc9ee6b19f05ea890249cf01575b24b958098
Tekletsadik Yigzaw <Tyigzaw@miu.edu>
2022-04-01 09:47:24 -0500

    Lab 4 Solution

--- CS315Assign/lab 3/assignm3.js
+++ CS315Assign/lab 3/assignm3.js
@@ -1,15 +1,11 @@
 "use strict";
-
-//JavaScript code For Calculator
-// let inp1 = document.querySelector(".first").value;
-// let inp2 = document.querySelector(".second").value;
+let inp1 = document.querySelector(".first").value;
+let inp2 = document.querySelector(".second").value;
 let add = document.querySelector(".plus");
-// let subtract = document.querySelector(".minus");
-// let multip = document.querySelector(".multi");
 let oper = document.querySelector(".result");
 add.addEventListener("click", function () {
   let sum;
-  // sum=Number(inp1) + Number(inp2) //I don't know why doesn't work
+  //  sum=Number(inp1) + Number(inp2) //I don't know why doesn't work
   sum =
     Number(document.querySelector(".first").value) +
     Number(document.querySelector(".second").value);
@@ -34,19 +30,7 @@ let multiply = function () {
   let multiplyTwo = input1 * input2;
   document.getElementById("operation").innerHTML = multiplyTwo;
 };
-
-///////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////;
-
-// //Reference
-// let elements = document.getElementById("myId");
-// elements.innerHTML = "Atnafu Yigzw muller piter";
-// console.log(elements.innerHTML);
-
-// let trial = document.querySelector(".MyClass");
-// trial.addEventListener("click", function () {
-//   trial.innerHTML = "tekletsdik@gma";
-//   trial.style.backgroundColor = "green";
-//   trial.style.color = "white";
-//   console.log(trial.innerHTM);
-// });
+let x = "";
+if (x === "") {
+  alert("No");
+}
