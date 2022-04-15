322e45296af1f09628913fb7457bb3718384caa1
Tekletsadik Yigzaw <Tyigzaw@miu.edu>
2022-04-15 09:53:49 -0500

    solution for Assignment

--- CS315Assign/lab 4/extendEvent.js
+++ CS315Assign/lab 4/extendEvent.js
@@ -62,35 +62,36 @@ function itemAdd() {
 
 function removeFun() {
   console.log(this);
-  // this.parentNode.parentNode.remove();
+  this.parentNode.parentNode.remove();
 }
-// function editFun() {
-//   let childernOfRow = this.parentNode.parentNode.childern;
-//   childernOfRow[0].remove();
-// }
-// function saveFun() {
-//   // console.log(this);
-//   // let childernOfRow = this.parentNode.parentNode.childern;
-//   // childernOfRow[0].prop("disabled", true);
-// }
+function editFun() {
+  let childernOfRow = this.parentNode.parentNode.childern;
+  childernOfRow[0].remove();
+}
+function saveFun() {
+  console.log(this);
+  let childernOfRow = this.parentNode.parentNode.childern;
+  childernOfRow[0].prop("disabled", true);
+}
 
-/* When the user clicks on the button,
-      toggle between hiding and showing the dropdown content */
-// function myFunction() {
-//   document.getElementById("myDropdown").classList.toggle("show");
-// }
-// function filterFunction() {
-//   let input, filter, ul, li, a, i;
-//   input = document.getElementById("myInput");
-//   filter = input.value.toUpperCase();
-//   div = document.getElementById("myDropdown");
-//   a = div.getElementsByTagName("a");
-//   for (i = 0; i < a.length; i++) {
-//     txtValue = a[i].textContent || a[i].innerText;
-//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//       a[i].style.display = "";
-//     } else {
-//       a[i].style.display = "none";
-//     }
-//   }
-// }
+// /* When the user clicks on the button,
+// // toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+function filterFunction() {
+  let input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+//
